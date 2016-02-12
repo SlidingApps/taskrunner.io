3d4147210ad7a6c6e8a4fbbe3088c9002b7a3afe
dev-slidingapps <slidingapps@gmail.com>
2016-02-12 17:24:58 +0100

    Added comments

--- src/SlidingApps.TaskRunner.Foundation.Configuration/ApplicationConfigurationStore.cs
+++ src/SlidingApps.TaskRunner.Foundation.Configuration/ApplicationConfigurationStore.cs
@@ -3,9 +3,17 @@ using System.Configuration;
 
 namespace SlidingApps.TaskRunner.Foundation.Configuration
 {
+    /// <summary>
+    /// App.config configuration reader.
+    /// </summary>
     public class ApplicationConfigurationStore
         : IApplicationConfigurationStore
     {
+        /// <summary>
+        /// Gets the app setting from the APP.CONFIG.
+        /// </summary>
+        /// <param name="key">The app setting key.</param>
+        /// <returns></returns>
         public string this[string key]
         {
             get
