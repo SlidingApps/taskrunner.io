df88ecc57953f76830b36793f46fae479a40e9ec
dev-slidingapps <slidingapps@gmail.com>
2016-03-05 18:35:31 +0100

    Database start schema

--- src/SlidingApps.TaskRunner.Script/DataDefinition/00011.CV.Organization_Summary_V.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE VIEW `Organization_Summary_V` AS
-SELECT org_hub.ID, org_hub.Code, org_sat.Name, org_sat.Description, org_sat.ValidFrom, org_sat.ValidUntil, COUNT(pers_hub.ID) AS NumberOfPersons
-FROM Organization_H org_hub
-JOIN Organization_S org_sat ON org_sat.OrganizationID = org_hub.ID
-JOIN Person_H pers_hub ON pers_hub.OrganizationID = org_hub.ID
-GROUP BY org_hub.ID;
