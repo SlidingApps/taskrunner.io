c4c85c9c0f56b4a94950df40321fe3ce185ad900
dev-slidingapps <slidingapps@gmail.com>
2016-05-03 21:55:15 +0200

    account forgot password

---
+++ src/SlidingApps.TaskRunner.Domain.WriteModel/Platform/EntityStatus.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SlidingApps.TaskRunner.Domain.WriteModel.Platform
+{
+    public enum EntityStatus
+    {
+        NotSpecified = 0,
+        Unconfirmed = 10,
+        Confirmed = 20
+    }
+}
