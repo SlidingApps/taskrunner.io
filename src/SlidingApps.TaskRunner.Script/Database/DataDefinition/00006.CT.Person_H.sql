2897ce37301b13c1e235965ca401ce9136cb9d1c
Peter Vyvey <peter.vyvey@gmail.com>
2017-02-08 14:08:52 +0100

    renamed sa.tr.wm.mail.* to sa.tr.wm.infrastructure.*

--- src/SlidingApps.TaskRunner.Script/Database/DataDefinition/00006.CT.Person_H.sql
+++ src/SlidingApps.TaskRunner.Script/Database/DataDefinition/00006.CT.Person_H.sql
@@ -1,13 +1,12 @@
 
 CREATE TABLE `Person_H` (
   `ID` CHAR(36) NOT NULL COMMENT '',
-  `TenantID` CHAR(36) NOT NULL COMMENT '',
+  `EmailAddress` varchar(200) NOT NULL,
   `StartDate` DATE NOT NULL DEFAULT '1900-01-01' COMMENT '',
   `EndDate` DATE NOT NULL DEFAULT '9999-01-01' COMMENT '',
   `_Modifier` VARCHAR(50) NOT NULL DEFAULT 'UNKNOWN' COMMENT '',
   `_Timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
   `_Version` INT NOT NULL DEFAULT 1 COMMENT '',
   PRIMARY KEY (`ID`)  COMMENT '',
-  INDEX `IDX_Person_H_TenantID` (`TenantID` ASC)  COMMENT '',
-  INDEX `IDX_Person_H_StartDate-EndDate` (`StartDate`, `EndDate` ASC)  COMMENT '',
-  FOREIGN KEY (`TenantID`) REFERENCES `Tenant_H` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE);
+  UNIQUE KEY `UDX_Account_H_Emailaddress` (`EmailAddress`),
+  INDEX `IDX_Person_H_StartDate-EndDate` (`StartDate`, `EndDate` ASC)  COMMENT '');
